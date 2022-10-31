ad3036215a6203f16bff33c0cf2f43bc0dc92632
桃花島主 <646263409@qq.com>
2022-11-01 00:03:38 +0800

    Create helloworld.vue

---
+++ helloworld.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    click() {}
+  }
+}
+</script>
+
+<template>
+  <button @click="click">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
